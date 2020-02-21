2695c4be68483759b767d44fdcb46f7613ea9c0d
Mark König <mark.koenig@kleiner-schelm.de>
2020-02-21 20:18:58 +0100

    Open / Close project

---
+++ CodeGeneratorOpenness/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CodeGeneratorOpenness
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open project";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Close project";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(162, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 147);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Device";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Code Gererator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CodeGeneratorOpenness/MainForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Siemens.Engineering;
+using Siemens.Engineering.HW;
+using Siemens.Engineering.HW.Features;
+using Siemens.Engineering.SW;
+using Siemens.Engineering.SW.Blocks;
+using Siemens.Engineering.SW.ExternalSources;
+using Siemens.Engineering.SW.Tags;
+using Siemens.Engineering.SW.Types;
+using Siemens.Engineering.Hmi;
+using HmiTarget = Siemens.Engineering.Hmi.HmiTarget;
+using Siemens.Engineering.Hmi.Tag;
+using Siemens.Engineering.Hmi.Screen;
+using Siemens.Engineering.Hmi.Cycle;
+using Siemens.Engineering.Hmi.Communication;
+using Siemens.Engineering.Hmi.Globalization;
+using Siemens.Engineering.Hmi.TextGraphicList;
+using Siemens.Engineering.Hmi.RuntimeScripting;
+using Siemens.Engineering.Compiler;
+using Siemens.Engineering.Library;
+using System.IO;
+
+namespace CodeGeneratorOpenness
+{
+    public partial class MainForm : Form
+    {
+        // just lazzy for now
+        public static TiaPortal tiaPortal = null;
+        public static Project project = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (project == null)
+            {
+                // see if we have a open instance and use the first one
+                foreach (TiaPortalProcess tiaPortalProcess in TiaPortal.GetProcesses())
+                {
+                    TiaPortalProcess p = TiaPortal.GetProcess(tiaPortalProcess.Id);
+
+                    tiaPortal = p.Attach();
+                    Console.WriteLine("TIA Portal has been attached");
+                    break;
+                }
+
+                // we dont habe a instance then open a new instance
+                if (tiaPortal == null)
+                {
+                    tiaPortal = new TiaPortal(TiaPortalMode.WithUserInterface);
+                    Console.WriteLine("TIA Portal has started");
+                }
+
+                // let's get the projects
+                ProjectComposition projects = tiaPortal.Projects;
+
+                // projects available ?
+                Console.WriteLine("TIA projects " + projects.Count.ToString());
+
+                // no open project the open dialog
+                if (projects.Count == 0)
+                {
+                    Console.WriteLine("Opening Project...");
+
+                    string filePath = string.Empty;
+                    using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                    {
+                        openFileDialog.Filter = "V16 project files (*.ap16)|*.ap16|All files (*.*)|*.*";
+                        openFileDialog.FilterIndex = 1;
+                        openFileDialog.RestoreDirectory = true;
+
+                        if (openFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            //Get the path of specified file
+                            filePath = openFileDialog.FileName;
+
+                            // load projectpath
+                            FileInfo projectPath = new FileInfo(filePath);
+                            try
+                            {
+                                project = projects.Open(projectPath);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine(String.Format("Could not open project {0}", projectPath.FullName));
+                                Application.Exit();
+                            }
+                        }
+                        else
+                            Application.Exit(); // no file selected
+                    }
+                }
+                else
+                {
+                    project = tiaPortal.Projects[0];
+                }
+
+                Console.WriteLine(String.Format("Project {0} is open", project.Path.FullName));
+
+                IterateThroughDevices(project);
+            }
+                 
+        }
+
+
+        private void IterateThroughDevices(Project project)
+        {
+            if (project == null)
+            {
+                Console.WriteLine("Project cannot be null");
+                return;
+            }
+            Console.WriteLine(String.Format("Iterate through {0} device(s)", project.Devices.Count));
+
+            foreach (Device device in project.Devices)
+            {
+                Console.WriteLine(String.Format("Device: \"{0}\".", device.Name));
+                listBox1.Items.Add(device.Name);
+            }
+            Console.WriteLine();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (project != null)
+            {
+                listBox1.Items.Clear();
+
+                project.Close();
+                project = null;
+            }
+        }
+    }
+}
